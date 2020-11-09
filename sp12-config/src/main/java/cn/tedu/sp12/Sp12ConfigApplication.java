bddb2d5d9f22bd4f5798fc1a2d827ee231f0c945
baidw1217 <18667115536@163.com>
2020-11-09 11:38:39 +0800

    增加zipkin配置信息

---
+++ sp12-config/src/main/java/cn/tedu/sp12/Sp12ConfigApplication.java
@@ -0,0 +1,15 @@
+package cn.tedu.sp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Sp12ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp12ConfigApplication.class, args);
+    }
+
+}
